330cd9ceb33f2f16eb133709492252cb2551ecba
Ashwin Saravanapavan <ashwinsiva13@gmail.com>
2020-09-03 20:12:22 -0400

    layout de l'application fini

--- app/src/main/java/com/example/laboratoire1sa/MainActivity.java
+++ app/src/main/java/com/example/laboratoire1sa/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.laboratoire1sa;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText et_motPasse = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
