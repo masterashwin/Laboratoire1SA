c36fc68657077c3f32b90c2bc5b6e14ad83ce000
Ashwin Saravanapavan <ashwinsiva13@gmail.com>
2020-09-09 17:45:29 -0400

    Bouton validation fonctionne avec changement de couleur et message

--- app/src/main/java/com/example/laboratoire1sa/MainActivity.java
+++ app/src/main/java/com/example/laboratoire1sa/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.laboratoire1sa;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.text.InputType;
+import android.text.SpannableString;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import java.util.regex.Pattern;
 
@@ -15,27 +19,28 @@ public class MainActivity extends AppCompatActivity {
 
     private static final Pattern SPECIFICATION_MOT_PASSE =
             Pattern.compile("^" +
-                    "(?=.*[0-9])" +         //at least 1 digit
-                    "(?=.*[a-z])" +         //at least 1 lower case letter
-                    "(?=.*[A-Z])" +         //at least 1 upper case letter
-                    "(?=.*[@#$%^&+=])" +    //at least 1 special character
-                    ".{10,}" +               //at least 4 characters
+                    "(?=.*[0-9])" +
+                    "(?=.*[a-z])" +
+                    "(?=.*[A-Z])" +
+                    "(?=.*[@#$%^&+=])" +
+                    ".{10,}" +
                     "$");
 
 
- //   @ # $ % & ( ) [ ] {
-  //  } _ = < > + - ! ? *
-   //         / | : ; . , ‘ " ~ ^
+    //@ # $ % & ( ) [ ] {
+   // } _ = < > + - ! ? *
+       //    / | : ; . , ‘ " ~ ^
 
     EditText et_motPasse;
     CheckBox chk_affichageMotPasse;
+    TextView tv_messageValidation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        tv_messageValidation = (TextView) findViewById(R.id.tv_messageValidation);
         et_motPasse = (EditText) findViewById(R.id.et_motPasse);
         chk_affichageMotPasse = (CheckBox) findViewById(R.id.chk_affichageMotPasse);
 
@@ -52,6 +57,16 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onClick_btn_validerMotPasse(View view) {
+        String motPasse = et_motPasse.getText().toString().trim();
+        if(SPECIFICATION_MOT_PASSE.matcher(motPasse).matches()){
+            tv_messageValidation.setText("Le mot de passe est valide");
+            tv_messageValidation.setTextColor(Color.rgb(0,255,0));
+        }else{
+            tv_messageValidation.setText("Le mot de passe n'est pas valide");
+            tv_messageValidation.setTextColor(Color.rgb(255,0,0));
+
+        }
+
 
 
     }
